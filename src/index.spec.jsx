a95c7d18375b8f9424b3843b12c90d6c4c00746f
Krzysztof Sztompka <sztompka.krzysztof@gmail.com>
2018-03-31 00:15:02 +0200

    init tests

---
+++ src/index.spec.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ExtensionerProvider, ExtensionerEvent } from "./index.js";
+import { Manager } from "extensioner";
+describe("base", () => {
+    it("should render extension component", () => {
+        const manager = new Manager();
+        manager.registerExtension({
+            events: {
+                "TEST_EVENT": value => <div className="TEST_COMPONENT">{value}</div>
+            }
+        });
+        const wrapper = Enzyme.mount(
+            <ExtensionerProvider manager={ manager }>
+                <ExtensionerEvent name="TEST_EVENT" value="TEST_VALUE" />
+            </ExtensionerProvider>
+        );
+        expect(wrapper.contains(<div className="TEST_COMPONENT">TEST_VALUE</div>)).to.be.true();
+    });
+});
--- webpack.test.babel.js
+++ webpack.test.babel.js
@@ -1,27 +1,8 @@
-import path from "path";
-
 export default {
     devtool: "source-map",
-    entry: "./src/index",
-
-    output: {
-        libraryTarget: "umd",
-        filename: "index.js",
-        path: path.resolve(__dirname, "dist")
-    },
     resolve: {
         extensions: [".js", ".jsx", ".json"]
     },
-    externals: [
-        {
-            react: {
-                root: "React",
-                commonjs2: "react",
-                commonjs: "react",
-                amd: "react"
-            }
-        }
-    ],
     module: {
         rules: [
             {
